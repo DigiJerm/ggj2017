4b943c16fb570413dbb9331f2e7e46d3290bc91d
Jerm <digijerm@gmail.com>
2017-01-20 21:57:59 +1300

    Draws a "line" made up of a bunch of points connected by straight line segments.

--- main.js
+++ main.js
@@ -4,6 +4,24 @@ window.onload = function()
 {
 	var canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
-	context.fillStyle = "green";
+	context.fillStyle = "#ffffff";
 	context.fillRect(0, 0, 800, 600);
+	var line = new Line();
+	context.strokeStyle = "#000000";
+	context.lineWidth = 3;
+	context.beginPath();
+	context.moveTo(800 / (line.NumPoints - 1) * 0, 300 + line.Offsets[0]);
+	for (var i = 1; i < line.NumPoints; i++)
+		context.lineTo(800 / (line.NumPoints - 1) * i, 300 + line.Offsets[i]);
+	context.stroke();
 };
+
+
+
+function Line()
+{
+	this.NumPoints = 30;
+	this.Offsets = [];
+	for (var i = 0; i < this.NumPoints; i++)
+		this.Offsets.push(Math.sin(i / (this.NumPoints - 1) * Math.PI * 2 * 3) * 30);
+}
