51d031a256125ccb84bd7b47ace70b97044cb909
DigiJerm <digijerm@gmail.com>
2017-01-20 22:27:23 +1300

    Added basic keyboard input (press Left to animate the line).

--- game.js
+++ game.js
@@ -5,8 +5,25 @@ function Game()
 
 
 
+Game.prototype.update = function()
+{
+	if (Key.isDown(Key.LEFT)) {
+		var temp = this.line.Offsets[0];
+		for (var i = 0; i < this.line.NumPoints - 1; i++)
+			this.line.Offsets[i] = this.line.Offsets[i + 1];
+		this.line.Offsets[this.line.NumPoints - 1] = temp;
+	}
+};
+
+
+
 Game.prototype.render = function()
 {
+	// clear background
+	context.fillStyle = "#ffffff";
+	context.fillRect(0, 0, 800, 600);
+
+	// draw line
 	context.strokeStyle = "#000000";
 	context.lineWidth = 3;
 	context.beginPath();
