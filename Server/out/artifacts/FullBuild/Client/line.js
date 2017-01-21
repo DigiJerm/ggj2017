47d825e2954ce5fe48a4649f8761f3cb5afd4382
DigiJerm <digijerm@gmail.com>
2017-01-21 19:26:06 +1300

    Added background image. Made game take up full width of browser window and changed resolution to 1920*900.
    Added shadow effect on conduit.

--- Server/out/artifacts/FullBuild/Client/line.js
+++ Server/out/artifacts/FullBuild/Client/line.js
@@ -3,5 +3,5 @@ function Line()
 	this.NumPoints = 30;
 	this.Offsets = [];
 	for (var i = 0; i < this.NumPoints; i++)
-		this.Offsets.push(Math.sin(i / (this.NumPoints - 1) * Math.PI * 2 * 3) * 30);
+		this.Offsets.push(Math.sin(i / (this.NumPoints - 1) * Math.PI * 2 * 3) * 50);
 }
